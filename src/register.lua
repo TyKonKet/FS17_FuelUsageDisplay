3036ac663d898e90962806722fb142af13707aaf
Matteo Autieri <matteoautieri@live.it>
2017-02-22 16:18:04 +0100

    remade the registration script

--- src/register.lua
+++ src/register.lua
@@ -1,79 +1,61 @@
 --
--- FuelUsageDisplay Registration
---
+-- BetterFuelUsage
 --
 -- @author  TyKonKet
 -- @date 27/10/2016
+BetterFuelUsageRH = {};
+BetterFuelUsageRH.name = "FuelUsageDisplayRH";
+BetterFuelUsageRH.specialization = {};
+BetterFuelUsageRH.specialization.title = "BetterFuelUsage";
+BetterFuelUsageRH.specialization.name = "betterFuelUsage";
+BetterFuelUsageRH.debug = true;
 
-RegistrationHelper_FUD = {};
-RegistrationHelper_FUD.isLoaded = false;
-RegistrationHelper_FUD.name = "FuelUsageDisplayRH";
-RegistrationHelper_FUD.specializationTitle = "FuelUsageDisplay";
-RegistrationHelper_FUD.specializationName = "fuelUsageDisplay";
-RegistrationHelper_FUD.specializationScript = g_currentModDirectory .. "fuelUsageDisplay.lua";
-RegistrationHelper_FUD.debug = false;
-
-function RegistrationHelper_FUD:print(txt)
-    if RegistrationHelper_FUD.debug then
-        print("[" .. self.name .. "] -> " .. txt);
-    end
-end
-
-if SpecializationUtil.specializations[RegistrationHelper_FUD.specializationName] == nil then
-    SpecializationUtil.registerSpecialization(RegistrationHelper_FUD.specializationName, RegistrationHelper_FUD.specializationTitle, RegistrationHelper_FUD.specializationScript)
-    RegistrationHelper_FUD.isLoaded = false;
-    RegistrationHelper_FUD:print("Specialization " .. RegistrationHelper_FUD.specializationTitle .. " registered!");
-end
-
-function RegistrationHelper_FUD:loadMap(name)
-    --DebugUtil.printTableRecursively(self, self.name .. " -> ", 0, 1);
-    if not g_currentMission.registrationHelper_FUD_isLoaded then
-        if not self.isLoaded then
-            self:register();
-        end
-        g_currentMission.registrationHelper_FUD_isLoaded = true;
-        self.isLoaded = true;
-        if self.debug then
-            addConsoleCommand("AAAFUDPrintVheicle", "", "FUDPrintVheicle", self);
-            addConsoleCommand("AAAFUDSetGraph", "", "FUDSetGraph", self);
-        end
-    else
-        self:print("Error: " .. self.specializationTitle .. " has been loaded already!");
-    end
-end
-
-function RegistrationHelper_FUD:deleteMap()
-    g_currentMission.registrationHelper_FUD_isLoaded = false;
-end
-
-function RegistrationHelper_FUD:keyEvent(unicode, sym, modifier, isDown)
-end
-
-function RegistrationHelper_FUD:mouseEvent(posX, posY, isDown, isUp, button)
-end
-
-function RegistrationHelper_FUD:update(dt)
-end
-
-function RegistrationHelper_FUD:draw()
-end
-
-function RegistrationHelper_FUD:register()
-    for _, vehicle in pairs(VehicleTypeUtil.vehicleTypes) do
-        if vehicle ~= nil then
-            if SpecializationUtil.hasSpecialization(SpecializationUtil.getSpecialization("motorized"), vehicle.specializations) then
-                table.insert(vehicle.specializations, SpecializationUtil.getSpecialization(self.specializationName));
-                self:print("Specialization " .. self.specializationTitle .. " registered on " .. vehicle.name);
-		    end     
+function BetterFuelUsageRH:print(txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9)
+    if self.debug then
+        local args = {txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9};
+        for i, v in ipairs(args) do
+            if v then
+                print("[" .. self.name .. "] -> " .. tostring(v));
+            end
         end
     end
 end
 
-function RegistrationHelper_FUD.FUDPrintVheicle(self)
-    --DebugUtil.printTableRecursively(FuelUsageDisplay.currentVehicle, "", 0, 1);
+function BetterFuelUsageRH:loadMap(name)
+    if self.debug then
+        addConsoleCommand("AAAFUDPrintVheicle", "", "FUDPrintVheicle", self);
+        addConsoleCommand("AAAFUDSetGraph", "", "FUDSetGraph", self);
+    end
+    for _, vehicleType in pairs(VehicleTypeUtil.vehicleTypes) do
+        if vehicleType ~= nil then
+            if SpecializationUtil.hasSpecialization(SpecializationUtil.getSpecialization("motorized"), vehicleType.specializations) then
+                table.insert(vehicleType.specializations, SpecializationUtil.getSpecialization(self.specialization.name));
+                self:print(self.specialization.title .. " added to " .. vehicleType.name);
+            end
+        end
+    end
 end
 
-function RegistrationHelper_FUD.FUDSetGraph(self, t1x, t1y, t1fs, t2fs)
+function BetterFuelUsageRH:deleteMap()
+end
+
+function BetterFuelUsageRH:keyEvent(unicode, sym, modifier, isDown)
+end
+
+function BetterFuelUsageRH:mouseEvent(posX, posY, isDown, isUp, button)
+end
+
+function BetterFuelUsageRH:update(dt)
+end
+
+function BetterFuelUsageRH:draw()
+end
+
+function BetterFuelUsageRH.FUDPrintVheicle(self)
+--DebugUtil.printTableRecursively(FuelUsageDisplay.currentVehicle, "", 0, 1);
+end
+
+function BetterFuelUsageRH.FUDSetGraph(self, t1x, t1y, t1fs, t2fs)
     if t1x == "-" then
         t1x = FuelUsageDisplay.fuelUsageText.text1.x;
     end
@@ -91,9 +73,7 @@ function RegistrationHelper_FUD.FUDSetGraph(self, t1x, t1y, t1fs, t2fs)
     FuelUsageDisplay.fuelUsageText.text1.fontsize = tonumber(t1fs);
     FuelUsageDisplay.fuelUsageText.text2.y = tonumber(t1y);
     FuelUsageDisplay.fuelUsageText.text2.fontsize = tonumber(t2fs);
-    return string.format( "x1 = %f, y1 = %f, fs1 = %f, y2 = %f, fs2 = %f", t1x, t1y, t1fs, t1y, t2fs);
+    return string.format("x1 = %f, y1 = %f, fs1 = %f, y2 = %f, fs2 = %f", t1x, t1y, t1fs, t1y, t2fs);
 end
 
-
-
-addModEventListener(RegistrationHelper_FUD)
+addModEventListener(BetterFuelUsageRH)
