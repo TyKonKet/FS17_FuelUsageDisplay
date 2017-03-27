c58e896046776ad12aece8230b9ecc73eba19cc2
Matteo Autieri <matteoautieri@live.it>
2017-03-27 22:17:59 +0200

    improved fuel usage with mowers

---
+++ src/extensions/Mower.lua
@@ -0,0 +1,27 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 27/03/2017
+function Mower:postLoad(savegame)
+    BetterFuelUsage.print("Mower extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Mower.getPtoPowerMultiplier);
+end
+
+function Mower:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    local mowerEffectActive = false;
+    for _, mowerEffect in pairs(self.mower.effects) do
+        if mowerEffect.isActive then
+            mowerEffectActive = true;
+            break;
+        end
+    end
+    if mowerEffectActive then
+        powerMultiplier = powerMultiplier + 0.5;
+    end
+    return powerMultiplier;
+end
--- src/extensions/PowerConsumer.lua
+++ src/extensions/PowerConsumer.lua
@@ -4,12 +4,27 @@
 -- @author TyKonKet
 -- @date 27/03/2017
 function PowerConsumer:postLoad(savegame)
+    BetterFuelUsage.print("PowerConsumer extension loaded on " .. self.typeName);
     local m = 1.25;
     local mp = 2;
-    BetterFuelUsage.print(string.format("self.powerConsumer.maxForce:%s -> %s", self.powerConsumer.maxForce, self.powerConsumer.maxForce * m));
+    --BetterFuelUsage.print(string.format("self.powerConsumer.maxForce:%s -> %s", self.powerConsumer.maxForce, self.powerConsumer.maxForce * m));
     self.powerConsumer.maxForce = self.powerConsumer.maxForce * m;
-    BetterFuelUsage.print(string.format("self.powerConsumer.forceFactor:%s -> %s", self.powerConsumer.forceFactor, self.powerConsumer.forceFactor * m));
+    --BetterFuelUsage.print(string.format("self.powerConsumer.forceFactor:%s -> %s", self.powerConsumer.forceFactor, self.powerConsumer.forceFactor * m));
     self.powerConsumer.forceFactor = self.powerConsumer.forceFactor * m;
-    BetterFuelUsage.print(string.format("self.powerConsumer.neededPtoPower:%s -> %s", self.powerConsumer.neededPtoPower, self.powerConsumer.neededPtoPower * mp));
+    --BetterFuelUsage.print(string.format("self.powerConsumer.neededPtoPower:%s -> %s", self.powerConsumer.neededPtoPower, self.powerConsumer.neededPtoPower * mp));
     self.powerConsumer.neededPtoPower = self.powerConsumer.neededPtoPower * mp;
+end
+
+function PowerConsumer:getConsumedPtoTorque()
+    if self:getDoConsumePtoPower() then
+        local m = self:getPtoPowerMultiplier();
+        if self.powerConsumer.ptoRpm > 0.001 then
+            return self.powerConsumer.neededPtoPower / (self.powerConsumer.ptoRpm * math.pi / 30) * m;
+        end
+    end
+    return 0;
+end
+
+function PowerConsumer:getPtoPowerMultiplier()
+    return 1;
 end
