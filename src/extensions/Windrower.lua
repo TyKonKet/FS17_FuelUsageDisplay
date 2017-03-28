d3ca9a1a94913dfcaf9fea3f3d5b6c38ce70e94a
Matteo Autieri <matteoautieri@live.it>
2017-03-28 17:47:46 +0200

    improved fuel usage of windrowers

---
+++ src/extensions/Windrower.lua
@@ -0,0 +1,23 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 27/03/2017
+function Windrower:postLoad(savegame)
+    BetterFuelUsage.print("Windrower extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Windrower.getPtoPowerMultiplier);
+end
+
+function Windrower:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    for _, ps in pairs(self.sortedWindrowerParticleSystems) do
+        if ps.isEnabled then
+            powerMultiplier = powerMultiplier + 0.25;
+            break;
+        end
+    end
+    return powerMultiplier;
+end
