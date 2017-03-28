a5b07518ba1cf6698f1daa1bdf9a94c3fccd3956
Matteo Autieri <matteoautieri@live.it>
2017-03-28 16:19:52 +0200

    improved fuel usage of balers

---
+++ src/extensions/Baler.lua
@@ -0,0 +1,21 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 27/03/2017
+function Baler:postPostLoad(savegame)
+    BetterFuelUsage.print("Baler extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Baler.getPtoPowerMultiplier);
+end
+Baler.postLoad = Utils.appendedFunction(Baler.postLoad, Baler.postPostLoad);
+
+function Baler:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    if self.baler.lastAreaBiggerZero then
+        powerMultiplier = powerMultiplier + 0.75;
+    end
+    return powerMultiplier;
+end
