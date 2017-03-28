828ea05f186a6daec3dffb7043bc3691fea10843
Matteo Autieri <matteoautieri@live.it>
2017-03-28 17:34:51 +0200

    improved fuel usage of tedders

---
+++ src/extensions/Tedder.lua
@@ -0,0 +1,23 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 27/03/2017
+function Tedder:postLoad(savegame)
+    BetterFuelUsage.print("Tedder extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Tedder.getPtoPowerMultiplier);
+end
+
+function Tedder:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    for _, ps in pairs(self.tedderParticleSystems) do
+        if ps.isEnabled then
+            powerMultiplier = powerMultiplier + 0.25;
+            break;
+        end
+    end
+    return powerMultiplier;
+end
