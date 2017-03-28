99101eea040e25701937afc81ee3b7ca12db5438
Matteo Autieri <matteoautieri@live.it>
2017-03-28 14:39:56 +0200

    improved cultivator fuel usage

---
+++ src/extensions/Cultivator.lua
@@ -0,0 +1,20 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 27/03/2017
+function Cultivator:postLoad(savegame)
+    BetterFuelUsage.print("Cultivator extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Cultivator.getPtoPowerMultiplier);
+end
+
+function Cultivator:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    if self.cultivatorHasGroundContact then
+        powerMultiplier = powerMultiplier + 0.35;
+    end
+    return powerMultiplier;
+end
