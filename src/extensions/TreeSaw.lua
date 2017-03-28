c8100777f9e2267ae8cb24ac8536c2cdafcd87e5
Matteo Autieri <matteoautieri@live.it>
2017-03-28 16:43:08 +0200

    improved fuel usage of tree saws

---
+++ src/extensions/TreeSaw.lua
@@ -0,0 +1,20 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 28/03/2017
+function TreeSaw:postLoad(savegame)
+    BetterFuelUsage.print("TreeSaw extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, TreeSaw.getPtoPowerMultiplier);
+end
+
+function TreeSaw:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    if self.treeSaw.cutTimer > 0 then
+        powerMultiplier = powerMultiplier + 2.5;
+    end
+    return powerMultiplier;
+end
