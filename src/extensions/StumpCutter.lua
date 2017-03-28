73c69bdf75e7968e08853b44c38aaafbd4a20ef7
Matteo Autieri <matteoautieri@live.it>
2017-03-28 16:42:54 +0200

    improved fuel usage od stump cutters

---
+++ src/extensions/StumpCutter.lua
@@ -0,0 +1,20 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 28/03/2017
+function StumpCutter:postLoad(savegame)
+    BetterFuelUsage.print("StumpCutter extension loaded on " .. self.typeName);
+    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, StumpCutter.getPtoPowerMultiplier);
+end
+
+function StumpCutter:getPtoPowerMultiplier(superFunc)
+    local powerMultiplier = 1;
+    if superFunc ~= nil then
+        powerMultiplier = superFunc(self);
+    end
+    if self.curSplitShape ~= nil then
+        powerMultiplier = powerMultiplier + 1.75;
+    end
+    return powerMultiplier;
+end
