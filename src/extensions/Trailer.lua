a85bbd0030cc09bd928ee231d93bde0ad5d86c09
Matteo Autieri <matteoautieri@live.it>
2017-03-30 22:33:20 +0200

    improved trailer

--- src/extensions/Trailer.lua
+++ src/extensions/Trailer.lua
@@ -11,12 +11,24 @@ function Trailer:postLoad(savegame)
 end
 
 function Trailer:getPtoPowerMultiplier(superFunc)
-    local powerMultiplier = 1;
+    local powerMultiplier = 0;
     if superFunc ~= nil then
         powerMultiplier = superFunc(self);
     end
-    if (self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING) and (self.getIsTurnedOn ~= nil and self:getIsTurnedOn()) then
-        powerMultiplier = powerMultiplier + 0.3;
+    if powerMultiplier == PowerConsumer:getPtoPowerMultiplier() then
+        powerMultiplier = 0;
+    end
+    if self.getIsTurnedOn ~= nil and self:getIsTurnedOn() then
+        powerMultiplier = powerMultiplier + 1;
+    end
+    if (self.tipState == Trailer.TIPSTATE_OPENING) then
+        powerMultiplier = powerMultiplier + Utils.lerp(0.05, 0.40, self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex));
+    end
+    if (self.tipState == Trailer.TIPSTATE_OPEN) then
+        powerMultiplier = powerMultiplier + Utils.lerp(0, 0.35, self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex));
+    end
+    if (self.tipState == Trailer.TIPSTATE_CLOSING) then
+        powerMultiplier = powerMultiplier + 0.05;
     end
     return powerMultiplier;
 end
@@ -26,8 +38,9 @@ function Trailer:getConsumedPtoTorque(superFunc)
     if superFunc ~= nil then
         torque = superFunc(self);
     end
+    local power = self:getUnitCapacity(self.trailer.fillUnitIndex) / 200;
     if torque == 0 and (self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING) then
-        torque = torque + (80 / (540 * math.pi / 30));
+        torque = torque + (20 + (80 *  self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex)) / (540 * math.pi / 30));
     end
     return torque;
 end
