cf479abefbd1efb0af2440c633d45933c3a83e7b
Matteo Autieri <matteoautieri@live.it>
2017-03-30 11:32:37 +0200

    cylindered now consumes motor power @mieperDE

---
+++ src/extensions/Cylindered.lua
@@ -0,0 +1,26 @@
+--
+-- Better Fuel Usage
+--
+-- @author TyKonKet
+-- @date 29/03/2017
+function Cylindered:postLoad(savegame)
+    BetterFuelUsage.print("Cylindered extension loaded on " .. self.typeName);
+    self.getConsumedPtoTorque = Utils.overwrittenFunction(self.getConsumedPtoTorque, Cylindered.getConsumedPtoTorque);
+end
+
+function Cylindered:getConsumedPtoTorque(superFunc)
+    local torque = 0;
+    if superFunc ~= nil then
+        torque = superFunc(self);
+    end
+    for _, tool in pairs(self.movingTools) do
+        if tool.axisActionIndex ~= nil then
+            local move, _ = InputBinding.getInputAxis(tool.axisActionIndex);
+            move = math.abs(move);
+            if not InputBinding.isAxisZero(move) then
+                torque = torque + (move * 25 / (540 * math.pi / 30));
+            end
+        end
+    end
+    return torque;
+end
