8b880c42f10ee055061963f9234c62fa8f8f01d8
Matteo Autieri <matteoautieri@live.it>
2017-03-30 18:04:45 +0200

    improved wood harvesters

--- src/extensions/WoodHarvester.lua
+++ src/extensions/WoodHarvester.lua
@@ -14,11 +14,11 @@ function WoodHarvester:getConsumedPtoTorque(superFunc)
         torque = superFunc(self);
     end
     if self:getIsTurnedOn() then
-        torque = torque + (60 / (540 * math.pi / 30));
+        torque = torque + ((200 * self.cutMaxRadius) / (540 * math.pi / 30));
         if self.cutParticleSystemsActive then
-            torque = torque + (80 / (540 * math.pi / 30));
+            torque = torque + ((400 * self.cutMaxRadius) / (540 * math.pi / 30));
         elseif self.isAttachedSplitShapeMoving then
-            torque = torque + (100 / (540 * math.pi / 30));
+            torque = torque + ((300 * self.cutMaxRadius) / (540 * math.pi / 30));
         end
     end
     return torque;
