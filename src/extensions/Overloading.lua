0345be6a9e4a0ef8a38002c7a653f4ec481fdfe0
Matteo Autieri <matteoautieri@live.it>
2017-03-31 20:40:21 +0200

    overloading :thumbsup:

--- src/extensions/Overloading.lua
+++ src/extensions/Overloading.lua
@@ -6,6 +6,7 @@
 function Overloading:postLoad(savegame)
     BetterFuelUsage.print("Overloading extension loaded on " .. self.typeName);
     self.getConsumedPtoTorque = Utils.overwrittenFunction(self.getConsumedPtoTorque, Overloading.getConsumedPtoTorque);
+    self.getPtoRpm = Utils.overwrittenFunction(self.getPtoRpm, Overloading.getPtoRpm);
 end
 
 function Overloading:getConsumedPtoTorque(superFunc)
@@ -14,7 +15,18 @@ function Overloading:getConsumedPtoTorque(superFunc)
         torque = superFunc(self);
     end
     if self.overloading.didOverload then
-        torque = torque + ((self.overloading.capacity / 3) / (540 * math.pi / 30));
+        torque = torque + ((self.overloading.capacity / 5) / (870 * math.pi / 30));
     end
     return torque;
+end
+
+function Overloading:getPtoRpm(superFunc)
+    local ptoRpm = 0;
+    if superFunc ~= nil then
+        ptoRpm = superFunc(self);
+    end
+    if self.overloading.didOverload then
+        ptoRpm = math.max(ptoRpm, 870);
+    end
+    return ptoRpm;
 end
