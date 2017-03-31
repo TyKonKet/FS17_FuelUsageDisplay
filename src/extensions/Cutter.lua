e77120d130ce259ca307430e2dd00707a1262f62
Matteo Autieri <matteoautieri@live.it>
2017-03-31 22:32:48 +0200

    cutters :thumbsup:

--- src/extensions/Cutter.lua
+++ src/extensions/Cutter.lua
@@ -6,6 +6,7 @@
 function Cutter:postLoad(savegame)
     BetterFuelUsage.print("Cutter extension loaded on " .. self.typeName);
     self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Cutter.getPtoPowerMultiplier);
+    self.getPtoRpm = Utils.overwrittenFunction(self.getPtoRpm, Cutter.getPtoRpm);
 end
 
 function Cutter:getPtoPowerMultiplier(superFunc)
@@ -20,4 +21,15 @@ function Cutter:getPtoPowerMultiplier(superFunc)
         powerMultiplier = powerMultiplier + 0.3;
     end
     return powerMultiplier;
+end
+
+function Cutter:getPtoRpm(superFunc)
+    local ptoRpm = 0;
+    if superFunc ~= nil then
+        ptoRpm = superFunc(self);
+    end
+    if self.reelStarted and self.powerConsumer ~= nil then
+        ptoRpm = math.max(ptoRpm, self.powerConsumer.ptoRpm);
+    end
+    return ptoRpm;
 end
