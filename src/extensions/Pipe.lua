0df2be6d35f97981641d24c9e3c5ac08474803f0
Matteo Autieri <matteoautieri@live.it>
2017-03-31 21:51:00 +0200

    marged overloading with pipe

--- src/extensions/Pipe.lua
+++ src/extensions/Pipe.lua
@@ -16,7 +16,10 @@ function Pipe:getConsumedPtoTorque(superFunc)
         torque = superFunc(self);
     end
     if self.pipeCurrentState == 0 then
-        torque = torque + ((self.overloading.capacity / 8) / (540 * math.pi / 30));
+        torque = torque + ((self.overloading.capacity / 8) / (430 * math.pi / 30));
+    end
+    if self:getOverloadingActive() then
+        torque = torque + ((self.overloading.capacity / 5) / (755 * math.pi / 30));
     end
     return torque;
 end
@@ -27,7 +30,14 @@ function Pipe:getPtoRpm(superFunc)
         ptoRpm = superFunc(self);
     end
     if self.pipeCurrentState == 0 then
-        ptoRpm = math.max(ptoRpm, 540);
+        ptoRpm = math.max(ptoRpm, 430);
+    end
+    if self:getOverloadingActive() then
+        if self.threshingScale ~= nil then
+            ptoRpm = math.max(ptoRpm, 530);
+        else
+            ptoRpm = math.max(ptoRpm, 980);
+        end   
     end
     return ptoRpm;
 end
