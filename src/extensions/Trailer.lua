e6fe32768b59af9ca1f61808d276abc5987f1491
Matteo Autieri <matteoautieri@live.it>
2017-04-02 15:09:18 +0200

    trailers :thumbsup:

--- src/extensions/Trailer.lua
+++ src/extensions/Trailer.lua
@@ -5,32 +5,8 @@
 -- @date 27/03/2017
 function Trailer:postLoad(savegame)
     --BetterFuelUsage.print("Trailer extension loaded on " .. self.typeName);
-    self.getPtoPowerMultiplier = Utils.overwrittenFunction(self.getPtoPowerMultiplier, Trailer.getPtoPowerMultiplier);
-    self.getDoConsumePtoPower = Utils.overwrittenFunction(self.getDoConsumePtoPower, Trailer.getDoConsumePtoPower);
     self.getConsumedPtoTorque = Utils.overwrittenFunction(self.getConsumedPtoTorque, Trailer.getConsumedPtoTorque);
-end
-
-function Trailer:getPtoPowerMultiplier(superFunc)
-    local powerMultiplier = 0;
-    if superFunc ~= nil then
-        powerMultiplier = superFunc(self);
-    end
-    if powerMultiplier == PowerConsumer:getPtoPowerMultiplier() then
-        powerMultiplier = 0;
-    end
-    if self.getIsTurnedOn ~= nil and self:getIsTurnedOn() then
-        powerMultiplier = powerMultiplier + 1;
-    end
-    if (self.tipState == Trailer.TIPSTATE_OPENING) then
-        powerMultiplier = powerMultiplier + Utils.lerp(0.05, 0.40, self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex));
-    end
-    if (self.tipState == Trailer.TIPSTATE_OPEN) then
-        powerMultiplier = powerMultiplier + Utils.lerp(0, 0.35, self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex));
-    end
-    if (self.tipState == Trailer.TIPSTATE_CLOSING) then
-        powerMultiplier = powerMultiplier + 0.05;
-    end
-    return powerMultiplier;
+    self.getPtoRpm = Utils.overwrittenFunction(self.getPtoRpm, Trailer.getPtoRpm);
 end
 
 function Trailer:getConsumedPtoTorque(superFunc)
@@ -39,16 +15,24 @@ function Trailer:getConsumedPtoTorque(superFunc)
         torque = superFunc(self);
     end
     local power = self:getUnitCapacity(self.trailer.fillUnitIndex) / 200;
-    if torque == 0 and (self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING) then
-        torque = torque + (10 + (power *  self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex)) / (540 * math.pi / 30));
+    local factor = self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex);
+    if self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING or (#self.tipAnimations < 2 and self.tipState == Trailer.TIPSTATE_OPEN) then
+        torque = torque + ((5 + (power * factor)) / (760 * math.pi / 30));
     end
     return torque;
 end
 
-function Trailer:getDoConsumePtoPower(superFunc)
-    local r = false;
+function Trailer:getPtoRpm(superFunc)
+    local ptoRpm = 0;
     if superFunc ~= nil then
-        r = superFunc(self);
+        ptoRpm = superFunc(self);
     end
-    return r or self.tipState ~= Trailer.TIPSTATE_CLOSED;
+    if self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING or (#self.tipAnimations < 2 and self.tipState == Trailer.TIPSTATE_OPEN) then
+        ptoRpm = math.max(ptoRpm, 760);
+    end
+    return ptoRpm;
+end
+
+function Trailer:onAttach(attacherVehicle)
+    self.attacherVehicle = attacherVehicle;
 end
