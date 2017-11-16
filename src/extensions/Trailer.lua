6fdb7af24955fb0de998ae6cb4c2789b82e12185
TyKonKet <matteoautieri@live.it>
2017-11-16 18:16:08 +0100

    fixed issues with trailers

--- src/extensions/Trailer.lua
+++ src/extensions/Trailer.lua
@@ -18,10 +18,12 @@ function Trailer:getConsumedPtoTorque(superFunc)
         torque = superFunc(self);
     end
     if self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING or (#self.tipAnimations < 2 and self.tipState == Trailer.TIPSTATE_OPEN) then
-        local power = self:getUnitCapacity(self.trailer.fillUnitIndex) / 200;
+        local power = Utils.getMotorPowerPercentage(self, 0.3, 30);
         local factor = self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex);
-        torque = torque + ((5 + (power * factor)) / (760 * math.pi / 30));
-    --    torque = torque + (self:getUnitCapacity(self.trailer.fillUnitIndex) / 500) / (760 * math.pi / 30));
+        if self.tipState == Trailer.TIPSTATE_CLOSING then
+            factor = 0;
+        end
+        torque = torque + ((power / 10 + power * factor) / (650 * math.pi / 30));
     end
     return torque;
 end
@@ -33,7 +35,10 @@ function Trailer:getPtoRpm(superFunc)
     end
     if self.tipState == Trailer.TIPSTATE_OPENING or self.tipState == Trailer.TIPSTATE_CLOSING or (#self.tipAnimations < 2 and self.tipState == Trailer.TIPSTATE_OPEN) then
         local factor = self:getUnitFillLevel(self.trailer.fillUnitIndex) / self:getUnitCapacity(self.trailer.fillUnitIndex);
-        ptoRpm = math.max(ptoRpm, 540 + 440 * factor);
+        if self.tipState == Trailer.TIPSTATE_CLOSING then
+            factor = 0;
+        end
+        ptoRpm = math.max(ptoRpm, 320 + 330 * factor);
     end
     return ptoRpm;
 end
